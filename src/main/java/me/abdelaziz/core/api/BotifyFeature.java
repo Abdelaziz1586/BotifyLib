171e4ad841312a3b0d5420ecb0856107a0b0004a
Abdelaziz1586 <gamingblueslurp@gmail.com>
2025-03-28 16:37:55 +0200

    Update

--- src/main/java/me/abdelaziz/core/api/BotifyFeature.java
+++ src/main/java/me/abdelaziz/core/api/BotifyFeature.java
@@ -4,14 +4,15 @@ import me.abdelaziz.core.builder.BotifyBot;
 
 public abstract class BotifyFeature {
 
-    private final double price;
     private final String name, description;
+    private final double price, hostingPrice;
 
-    public BotifyFeature(final String name, final String description, final double price) {
+    public BotifyFeature(final String name, final String description, final double price, final double hostingPrice) {
         this.name = name.replaceAll("[^a-zA-Z]", "").toLowerCase();
 
         this.price = price;
         this.description = description;
+        this.hostingPrice = hostingPrice;
     }
 
     public void onEnable() {}
@@ -32,5 +33,13 @@ public abstract class BotifyFeature {
 
     public String getDescription() {
         return description;
+    }
+
+    public double getHostingPrice() {
+        return hostingPrice;
+    }
+
+    public double getTotalPrice() {
+        return price + hostingPrice;
     }
 }
