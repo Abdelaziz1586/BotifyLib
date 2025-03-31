31673bf4680d27934dbd82aac0af757d8b152dac
Abdelaziz1586 <gamingblueslurp@gmail.com>
2025-03-31 23:29:19 +0200

    API Update

--- src/main/java/me/abdelaziz/core/api/BotifyFeature.java
+++ src/main/java/me/abdelaziz/core/api/BotifyFeature.java
@@ -1,12 +1,19 @@
 package me.abdelaziz.core.api;
 
 import me.abdelaziz.core.builder.BotifyBot;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public abstract class BotifyFeature {
 
     private final double price, hostingPrice;
+    private final List<CommandData> commands;
     private final String key, name, description;
+    private final List<ListenerAdapter> listeners;
 
     public BotifyFeature(final String key, final String name, final String description, final double price, final double hostingPrice) {
         this.name = name;
@@ -15,6 +22,9 @@ public abstract class BotifyFeature {
         this.price = price;
         this.description = description;
         this.hostingPrice = hostingPrice;
+
+        this.commands = new ArrayList<>();
+        this.listeners = new ArrayList<>();
     }
 
     public void onEnable() {}
@@ -25,11 +35,37 @@ public abstract class BotifyFeature {
 
     public void onBotRemove(final BotifyBot botifyBot) {}
 
+    public final void registerListener(final ListenerAdapter... listeners) {
+        if (listeners == null) return;
+
+        for (final ListenerAdapter listener : listeners)
+            if (listener != null && !this.listeners.contains(listener))
+                this.listeners.add(listener);
+    }
+
+    public final void unregisterListener(final ListenerAdapter... listeners) {
+        if (listeners != null)
+            this.listeners.removeAll(List.of(listeners));
+    }
+
+    public final void registerCommand(final CommandData... commands) {
+        if (commands == null) return;
+
+        for (final CommandData command : commands)
+            if (command != null && !this.commands.contains(command))
+                this.commands.add(command);
+    }
+
+    public final void unregisterCommand(final CommandData... commands) {
+        if (commands != null)
+            this.commands.removeAll(List.of(commands));
+    }
+
     public final String getKey() {
         return key;
     }
 
-    public String getName() {
+    public final String getName() {
         return name;
     }
 
@@ -43,6 +79,14 @@ public abstract class BotifyFeature {
 
     public final double getHostingPrice() {
         return hostingPrice;
+    }
+
+    public List<CommandData> getCommands() {
+        return commands;
+    }
+
+    public List<ListenerAdapter> getListeners() {
+        return listeners;
     }
 
     public final double getTotalPrice() {
